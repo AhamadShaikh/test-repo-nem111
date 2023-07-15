7aadb5355077ad1e2f14492b2137ff1b87b1c663
AhamadShaikh <shaikhahamadrasul786@gmail.com>
2023-07-15 18:07:26 +0530

    backend-server

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const app = express();
 require("dotenv").config()
 const userRouter = require("./routes/userRoutes")
 const postRouter = require("./routes/postRoutes")
+const cors = require("cors")
 
 app.use(express.json())
+app.use(express.text());
+app.use(cors())
 
 const connection = async () => {
     try {
@@ -17,6 +20,10 @@ const connection = async () => {
 
 app.use("/user", userRouter)
 app.use("/posts", postRouter)
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 
 app.listen(7000, () => {
     try {
